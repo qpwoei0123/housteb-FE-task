09cb58943a768c9d9a505bd081b414256c7dfb60
qpwoei0123 <qpwoei0123@naver.com>
2024-01-12 15:24:57 +0900

    design: 전역스타일 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import Router from "./Router";
+import { styled, createGlobalStyle } from "styled-components";
 
 function App() {
   return (
-    <>
+    <Container>
+      <GlobalStyles />
       <Router />
-    </>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
 export default App;

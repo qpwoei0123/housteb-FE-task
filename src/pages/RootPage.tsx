68cd0822e0447936d6d35d8ae83892d8d2351b1d
qpwoei0123 <qpwoei0123@naver.com>
2024-01-12 16:01:02 +0900

    feat: RootPage 구현

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+import logo from "../assets/logo-black.png";
+import { Link } from "react-router-dom";
+
 const RootPage = () => {
-  return <>root</>;
+  return (
+    <Container className="root-page">
+      <img src={logo} alt="logo" />
+      <Button to={"/order"}>
+        <ButtonText>주문하러 가기</ButtonText>
+      </Button>
+    </Container>
+  );
 };
 
+const Container = styled.div`
+  width: 300px;
+  height: 100%;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
+
+const Button = styled(Link)`
+  width: 150px;
+  height: 80px;
+  border-radius: 20px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration-line: none;
+`;
+
+const ButtonText = styled.p`
+  color: black;
+  font-size: x-large;
+`;
 export default RootPage;

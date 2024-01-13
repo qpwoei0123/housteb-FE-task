a80b346a3630e6308b64290e3d9dc7d58c48f0a5
qpwoei0123 <qpwoei0123@naver.com>
2024-01-13 18:38:37 +0900

    feat: OrderPage 구조 구현

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ItemBox from "../components/ItemBox";
+
 const OrderPage = () => {
-  return <>order</>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+      </Container>
+      <Footer />
+    </>
+  );
 };
+
+const Container = styled.div`
+  width: 300px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 70px;
+  margin-bottom: 170px;
+  gap: 15px;
+`;
 
 export default OrderPage;

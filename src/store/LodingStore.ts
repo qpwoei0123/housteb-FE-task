f37e1947077040a7d265442e1346bcb4d1b430da
qpwoei0123 <qpwoei0123@naver.com>
2024-01-14 04:50:28 +0900

    feat: 로딩 컴포넌트 구현

---
+++ src/store/LodingStore.ts
@@ -0,0 +1,13 @@
+import create from "zustand";
+
+type LodingStoreProps = {
+  loading: boolean;
+  setLoading: (value: boolean) => void;
+};
+
+const useLoadingStore = create<LodingStoreProps>((set) => ({
+  loading: false,
+  setLoading: (value: boolean) => set({ loading: value }),
+}));
+
+export default useLoadingStore;

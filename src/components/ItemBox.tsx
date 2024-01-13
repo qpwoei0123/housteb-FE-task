d55a61b58b2b6b2d280f6a82bd474472690be4c7
qpwoei0123 <qpwoei0123@naver.com>
2024-01-14 05:00:28 +0900

    feat: 기능 구현 및 전역변수 적용

--- src/components/ItemBox.tsx
+++ src/components/ItemBox.tsx
@@ -1,30 +1,67 @@
 import styled from "styled-components";
 import EventChecker from "./EventChecker";
+import { Item } from "../types/item";
+import { useEffect, useState } from "react";
+import useTotalPriceStore from "../store/TotalPriceStore";
+import useTotalQuantityStore from "../store/TotalQuantityStore";
 
-const ItemBox = () => {
+const ItemBox: React.FC<Item> = ({ name, event, materialType, price }) => {
+  const [quantity, setQuantity] = useState<number>(0);
+  const addToTotalPrice = useTotalPriceStore((state) => state.addTotalPrice);
+  const subtractTotalPrice = useTotalPriceStore(
+    (state) => state.subtractTotalPrice
+  );
+  const addTotalQuantity = useTotalQuantityStore(
+    (state) => state.addTotalQuantity
+  );
+  const subtractTotalQuantity = useTotalQuantityStore(
+    (state) => state.subtractTotalQuantity
+  );
+
+  const handlePlus = () => {
+    if (quantity < 999) {
+      setQuantity(quantity + 1);
+      addToTotalPrice(price);
+      addTotalQuantity();
+    }
+  };
+  const handleMinus = () => {
+    if (quantity) {
+      setQuantity(quantity - 1);
+      subtractTotalPrice(price);
+      subtractTotalQuantity();
+    }
+  };
+  useEffect(() => {
+    console.log(quantity * price);
+  }, [quantity]);
   return (
-    <Container>
+    <Container hasBackground={quantity > 0}>
       <ItemImg />
       <ItemContentsDiv>
         <FlexBox>
-          <span>name</span>
-          <EventChecker />
+          <span>{name}</span>
+          {event ? <EventChecker /> : null}
         </FlexBox>
         <BetweenBox>
-          <span>- 1 +</span>
-          <span>price</span>
+          <QuantityBox>
+            <button onClick={handleMinus}>-</button>
+            <span>{quantity}</span>
+            <button onClick={handlePlus}>+</button>
+          </QuantityBox>
+          <span>{`${price.toLocaleString("ko-KR")}원`}</span>
         </BetweenBox>
       </ItemContentsDiv>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ hasBackground: boolean }>`
   width: 250px;
   height: 65px;
   border: 1px solid rgba(0, 0, 0, 0.3);
   border-radius: 15px;
-  background-color: white;
+  background-color: ${(props) => (props.hasBackground ? "#f75a2f3c" : "white")};
   display: flex;
   padding: 10px;
   gap: 15px;
@@ -46,6 +83,7 @@ const ItemContentsDiv = styled.div`
 const BetweenBox = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const FlexBox = styled.div`
@@ -53,4 +91,17 @@ const FlexBox = styled.div`
   align-items: center;
   gap: 10px;
 `;
+
+const QuantityBox = styled.div`
+  button {
+    border: none;
+    cursor: pointer;
+    background: none;
+    font-size: 18px;
+  }
+  span {
+    padding: 0 5px;
+  }
+`;
+
 export default ItemBox;
